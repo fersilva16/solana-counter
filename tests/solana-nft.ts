8e437d0660b45a57b34bf58af0bc56cebae38dfb
Fernando Silva <fernando457829@gmail.com>
2022-01-29 19:21:44 -0300

    feat: add basic counter

--- tests/solana-nft.ts
+++ tests/solana-nft.ts
@@ -1,17 +1,49 @@
+import assert from 'assert';
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { SolanaNft } from '../target/types/solana_nft';
 
 describe('solana-nft', () => {
+  const provider = anchor.Provider.env();
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaNft as Program<SolanaNft>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  let baseAccount: anchor.web3.Keypair | undefined = undefined;
+
+  it('creates a counter', async () => {
+    baseAccount = anchor.web3.Keypair.generate();
+
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey,
+    );
+
+    assert.ok(account.count.toNumber() === 0);
+  });
+
+  it('increments a counter', async () => {
+    assert.ok(baseAccount !== undefined);
+
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey,
+    );
+
+    assert.ok(account.count.toNumber() === 1);
   });
 });
